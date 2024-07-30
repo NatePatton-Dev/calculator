ecfe2938e27eb9e3e4ba55f210ac974974f0842e
Nate Patton <170039653+NatePatton-Dev@users.noreply.github.com>
2024-07-30 10:16:22 -0500

    add remaining button functionality/troubleshooting

--- script.js
+++ script.js
@@ -2,24 +2,115 @@ let displayValue = document.querySelector(".displaytext");
 
 let numButtons = document.querySelectorAll('.number');
 
+let numDotButtonCount = 0;
+
 for (i of numButtons) {
   i.addEventListener('click', function(e) {
-    displayValue.textContent += e.target.textContent;
+    if (!operatorChoice) {
+        if (displayValue.textContent === "0") {
+            displayValue.textContent = e.target.textContent;
+        } else {    
+        displayValue.textContent += e.target.textContent;
+        };
+    } else if (!secondNum) {
+        if (numDotButtonCount === 0) {
+            displayValue.textContent = e.target.textContent;
+            numDotButtonCount++;
+        } else {
+            displayValue.textContent += e.target.textContent;
+        }
+        // first numbutton or dot press replaces displayValue.textConter, unless dot, then "0."
+        // subsequent numbutton presses behave like normal
+    };
   });
 };
 
 let dot = document.querySelector("#dot");
 
 dot.addEventListener('click', function(e) {
-    if (!displayValue.textContent.includes(".")) {
+    if (!operatorChoice) {
+        if (!displayValue.textContent.includes(".")) {
         displayValue.textContent += e.target.textContent;
+        };
+    } else if (!secondNum) {
+        if (numDotButtonCount === 0) {
+            displayValue.textContent = "0.";
+            numDotButtonCount++;
+        } else {
+            if (!displayValue.textContent.includes(".")) {
+                displayValue.textContent += e.target.textContent;
+            };
+        };
+    };
+});
+
+let opButtons = document.querySelectorAll('.operator');
+
+for (i of opButtons) {
+  i.addEventListener('click', function(e) {
+    if (!!displayValue.textContent && !firstNum) {
+        firstNum = displayValue.textContent;
+        operatorChoice = e.target.textContent;
+        numDotButtonCount = 0;  
+    } else if (!!displayValue.textContent && !!firstNum) {
+        if (+displayValue.textContent === 0 && operatorChoice === "/") {
+            alert("Silly human please don't divide by 0");
+            displayValue.textContent = "0";
+            numDotButtonCount = 0;
+        } else {
+            secondNum = displayValue.textContent;
+            displayValue.textContent = operate(firstNum, operatorChoice, secondNum);
+            firstNum = displayValue.textContent;
+            operatorChoice = e.target.textContent;
+            secondNum = "";
+            numDotButtonCount = 0;
+        };
+    };
+  });
+};
+//if displayValue is negative, pressing any operator or equals changes display
+//value to 0
+
+let equals = document.querySelector("#equals");
+
+equals.addEventListener('click', function() {
+    if (!!firstNum && !!operatorChoice && !!displayValue.textContent) {
+        if (+displayValue.textContent === 0 && operatorChoice === "/") {
+            alert("Silly human please don't divide by 0");
+            displayValue.textContent = "0";
+            numDotButtonCount = 0;
+        } else {
+            secondNum = displayValue.textContent;
+            displayValue.textContent = operate(firstNum, operatorChoice, secondNum);
+            firstNum = displayValue.textContent;
+            secondNum = "";
+            numDotButtonCount = 0;
+        };
+        // need to be able to keep hitting = button to add the entered 
+        // secondNum to the displayed firstNum, similar problem with operator
+        // buttons
+    };
+});
+
+let backspace = document.querySelector("#delete");
+
+backspace.addEventListener('click', function() {
+    if (displayValue.textContent !== "0" && numDotButtonCount !== 0) {
+        let newDisplayValue = displayValue.textContent.substring(0, displayValue.textContent.length-1);
+        if (newDisplayValue === "") {
+            newDisplayValue = "0";
+        };
+        displayValue.textContent = newDisplayValue;
     };
 });
 
 let clear = document.querySelector("#clear");
 
 clear.addEventListener('click', function() {
-    displayValue.textContent = "";
+    displayValue.textContent = "0";
+    firstNum = "";
+    operatorChoice = "";
+    secondNum = "";
 });
 
 function add(a, b) {
@@ -38,23 +129,25 @@ function divide(a, b) {
     return a / b;
 };
 
-let first 
-let operator
-let second
+let firstNum 
+let operatorChoice
+let secondNum
 
 function operate(first, operator, second) {
     switch (operator) {
         case "+":
-            add(first, second);
+            return add(Number(first), Number(second));
             break;
         case "-":
-            subtract(first, second);
+            return subtract(Number(first), Number(second));
             break;
         case "*":
-            multiply(first, second);
+            return multiply(Number(first), Number(second));
             break;
         case "/":
-            divide(first, second);
+            return divide(Number(first), Number(second));
             break;
     };
 };
+
+//would like current operatorChoice to be highlighted
